85ca4cc4f4e19a20e766fc767b7aa9251a84f951
PNikray <poujanik@gmail.com>
2015-02-09 22:18:14 +0100

    added the install script that creates the shell script which runs the app

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Installing all the necessary dependencies"
+echo "-----------------------------------------"
+npm install
+echo "-----------------------------------------"
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+SHELLSCRIPT="#!/bin/sh\nnode $SCRIPTPATH/src/app.js $*\n"
+
+echo -e $SHELLSCRIPT >> jira-terminal
+chmod +x jira-terminal
+
+if [ "$(whoami)" != "root" ]; then
+	echo "Im not root. So please move the shell script 'jira-terminal' to your bin folder" 
+else
+	echo "Copying 'jira-terminal' to /usr/bin/"
+	mv jira-terminal.sh /usr/bin/jira-terminal
+fi
+echo "------------------------------------------"
+echo "You still have to configure the config.json file. See example.config.json."
