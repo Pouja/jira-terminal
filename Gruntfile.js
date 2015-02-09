b943ac6dff53779ac8f20ef0945a29293de335dd
PNikray <poujanik@gmail.com>
2015-02-09 15:26:04 +0100

    Added a simple install grunt task for makign an shell script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,8 @@
 'use strict';
+var prompt = require('prompt');
+var Q = require('q');
+var fs = require('fs');
+var util = require('util');
 
 module.exports = function(grunt) {
     require('time-grunt')(grunt);
@@ -30,6 +34,45 @@ module.exports = function(grunt) {
                 src: ['test/**/*.js']
             }
         }
+    });
+
+    var createExec = function(path) {
+        var deferred = Q.defer();
+        var file = [
+            '#!/bin/zsh',
+            'node ' + __dirname + '/src/app.js $*',
+            ''
+        ].join('\n');
+
+        if (path[path.length - 1] !== '/') {
+            path += '/';
+        }
+        var fullPath = path + 'jira-terminal';
+        Q.ninvoke(fs, 'writeFile', fullPath, file, 'utf8')
+            .then(function() {
+                deferred.resolve(fullPath);
+            }, deferred.reject);
+        return deferred.promise;
+    }
+
+    grunt.registerTask('install', function() {
+        var done = this.async();
+
+        prompt.start();
+        prompt.message = '';
+        prompt.delimiter = ':';
+        prompt.colors = false;
+        var questions = ['installation directory'];
+
+
+        Q.ninvoke(prompt, 'get', questions)
+            .then(function(result) {
+                return createExec(result[questions[0]]);
+            }).then(function(fullpath) {
+                grunt.log.write(util.format('Wrote executable to %s.', fullpath));
+                grunt.log.write('You still have to make it executable by for example performing chmod u+x.');
+                done();
+            }, done);
     });
     grunt.registerTask('default', function() {
         if (process.env.NODE_ENV !== 'test') {
