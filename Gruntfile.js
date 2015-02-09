4f269185a740c4558c48ee6edd996876b7227678
PNikray <poujanik@gmail.com>
2015-02-09 22:17:57 +0100

    made gruntfile cleaner by using grunt-env and creating and install script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,9 @@
-'use strict';
-var prompt = require('prompt');
-var Q = require('q');
-var fs = require('fs');
-var util = require('util');
-
 module.exports = function(grunt) {
     require('time-grunt')(grunt);
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-env');
 
     grunt.initConfig({
         jshint: {
@@ -17,7 +12,7 @@ module.exports = function(grunt) {
                 reporter: require('jshint-stylish')
             },
             all: {
-                src: ['src/**/*.js']
+                src: ['Gruntfile.js', 'src/**/*.js']
             },
             test: {
                 options: {
@@ -33,52 +28,13 @@ module.exports = function(grunt) {
                 },
                 src: ['test/**/*.js']
             }
-        }
+        },
+        env: {
+            test: {
+                NODE_ENV: 'test',
+            }
+        },
     });
 
-    var createExec = function(path) {
-        var deferred = Q.defer();
-        var file = [
-            '#!/bin/zsh',
-            'node ' + __dirname + '/src/app.js $*',
-            ''
-        ].join('\n');
-
-        if (path[path.length - 1] !== '/') {
-            path += '/';
-        }
-        var fullPath = path + 'jira-terminal';
-        Q.ninvoke(fs, 'writeFile', fullPath, file, 'utf8')
-            .then(function() {
-                deferred.resolve(fullPath);
-            }, deferred.reject);
-        return deferred.promise;
-    }
-
-    grunt.registerTask('install', function() {
-        var done = this.async();
-
-        prompt.start();
-        prompt.message = '';
-        prompt.delimiter = ':';
-        prompt.colors = false;
-        var questions = ['installation directory'];
-
-
-        Q.ninvoke(prompt, 'get', questions)
-            .then(function(result) {
-                return createExec(result[questions[0]]);
-            }).then(function(fullpath) {
-                grunt.log.write(util.format('Wrote executable to %s.', fullpath));
-                grunt.log.write('You still have to make it executable by for example performing chmod u+x.');
-                done();
-            }, done);
-    });
-    grunt.registerTask('default', function() {
-        if (process.env.NODE_ENV !== 'test') {
-            grunt.log.warn('Please run the tests with NODE_ENV=test to prevent seeing all the pretty tables and logs.');
-        } else {
-            grunt.task.run(['jshint', 'mochaTest']);
-        }
-    });
-}
+    grunt.registerTask('default', ['env:test', 'jshint', 'mochaTest']);
+};
