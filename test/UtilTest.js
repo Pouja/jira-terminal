34a8de8bec0b7ffc1b03ded188953fe5a5240a81
PNikray <poujanik@gmail.com>
2015-02-25 16:33:51 +0100

    added empty test suit for Util

---
+++ test/UtilTest.js
@@ -0,0 +1,74 @@
+//var Util = require('../src/Util.js');
+var assert = require('assert');
+
+describe('Util', function(){
+    describe('#hellp', function(){
+        it('Should call log with the correct table', function(){
+            assert(false);
+        });
+    });
+    describe('#createAsciiTable', function(){
+        it('Should call log with the correct table', function(){
+            assert(false);
+        });
+        it('Should do nothing when head or rows is not', function(){
+            assert(false);
+        });
+    });
+    describe('#setLinebreaks', function(){
+        it('Should correctly break the sentence with enters', function(){
+            assert(false);
+        });
+    });
+    describe('#cleanSentence', function(){
+        it('Should remove all the unwanted characers', function(){
+            assert(false);
+        });
+    });
+    describe('#_filter', function(){
+        it('Should throw when an boolean is given as filter', function(){
+            assert(false);
+        });
+        it('Should throw when an zero length string is given as filter', function(){
+            assert(false);
+        });
+        it('Should filter in a single column when \':\' is present', function(){
+            assert(false);
+        });
+        it('Should filter on all columsn \':\' is not present.', function(){
+            assert(false);
+        });
+    });
+    describe('#_sort', function(){
+        it('Should not do anything when the given column name does not exist', function(){
+            assert(false);
+        });
+        it('Should sort on the given column name', function(){
+            assert(false);
+        });
+    });
+    describe('#makeIssueLink', function(){
+        it('Should give a pretty link with string as argument', function(){
+            assert(false);
+        });
+        it('Should give a pretty link with object as argument', function(){
+            assert(false);
+        });
+        it('Should throw on all other types of argument', function(){
+            assert(false);
+        });
+    });
+    describe('#makeVerticalRows', function(){
+        it('Should make a correct vertical table', function(){
+            assert(false);
+        });
+    });
+    describe('#branch', function(){
+        it('Should do nothing when branch flag is not set', function(){
+            assert(false);
+        });
+        it('Should call make branch and checkout with the correct arguments', function(){
+            assert(false);
+        });
+    });
+});
