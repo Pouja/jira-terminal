483ed23ec88a25aa3c054ae52f5416e23bf5f2e6
PNikray <poujanik@gmail.com>
2015-03-20 12:18:11 +0100

    Addech check for $EDITOR and vim.
    Ubuntu users dont have the $EDITOR env set by default and they dont have
    vim. This resulted in an ENOENT when the editor package tried to open
    vim.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 var fs = require('fs');
+var which = require('which');
 
 // Make sure the user does not run with sudo since we spawn shell commands.
 var uid = parseInt(process.env.SUDO_UID);
 if (uid) {
     throw 'You are not allowed to run with sudo!\nThis is done for security reasons!!';
+}
+
+// Check if the editor package will even work
+if (!process.env.EDITOR) {
+    try {
+        which.sync('vim');
+    } catch (e) {
+        throw 'Your $EDITOR is not set and you don\'t have vim installed.\n' +
+            'Please either set your env $EDITOR or install vim.';
+    }
 }
 
 // Check for the existence of the config file.
