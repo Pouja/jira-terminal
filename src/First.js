8a7fa06db09fedc47be0b458c942072b99257d05
PNikray <poujanik@gmail.com>
2015-03-29 21:32:01 +0200

    Added promp for first time use.
    It will now ask the information for the configuration file.
    This is a setup for the rewriting the app for using oauth1.

---
+++ src/First.js
@@ -0,0 +1,49 @@
+var prompt = require('cli-prompt');
+var _ = require('lodash');
+var fs = require('fs');
+
+/**
+* This will run on the first time.
+* It will set the password and username etc.
+*/
+module.exports.run = function(cb) {
+    var config = {
+        'protocol': 'https',
+        'host': 'jira.example.nl',
+        'port': 443,
+        'username': 'john.lee',
+        'password': 'secret',
+    };
+
+    console.log('You will be prompt to fill in some information.');
+    console.log('These information are necessary to run the application.');
+
+    prompt.multi([{
+        key: 'host',
+        label: 'host (jira.example.com)'
+    }, {
+        key: 'username'
+    }, {
+        key: 'password',
+        label: 'password (currently this will be save as plain text)',
+        type: 'password'
+    }, {
+        key: 'path',
+        label: 'path (the absolute path to store the config file)'
+    }], function(answers) {
+        var filename = 'jira-terminal-config.json';
+
+        var path = answers.path;
+        path += (path[path.length - 1] === '/') ? filename : '/' + filename ;
+        delete answers.path;
+
+        _.assign(config, answers);
+
+        console.log('Writing the configuration file to ' + path);
+        fs.writeFileSync(path, JSON.stringify(config), 'utf8');
+        fs.writeFileSync('./config', answers.path, 'utf8');
+
+        console.log('Starting the app.');
+        cb(path);
+    });
+}
--- src/app.js
+++ src/app.js
@@ -1,30 +1,64 @@
 var fs = require('fs');
 var which = require('which');
 
+/**
+* Start the app.
+*/
+var start = function(configLocation) {
+    try {
+        var config = JSON.parse(fs.readFileSync(configLocation, 'utf8'));
+        require('./PluginLoader.js').run(config);
+
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+        console.error('It seems that you removed the configuration file.\nPlease restart the application');
+        fs.unlinkSync('./config');
+    }
+}
+
 // Make sure the user does not run with sudo since we spawn shell commands.
-var uid = parseInt(process.env.SUDO_UID);
-if (uid) {
-    throw 'You are not allowed to run with sudo!\nThis is done for security reasons!!';
+var checkSudo = function() {
+    var uid = parseInt(process.env.SUDO_UID);
+    if (uid) {
+        throw 'You are not allowed to run with sudo!\nThis is done for security reasons!!';
+    }
 }
 
 // Check if the editor package will even work
-if (!process.env.EDITOR) {
+var checkEditor = function() {
+    if (!process.env.EDITOR) {
+        try {
+            which.sync('vim');
+        } catch (e) {
+            throw 'Your $EDITOR is not set and you don\'t have vim installed.\n' +
+                'Please either set your env $EDITOR or install vim.';
+        }
+    }
+}
+
+/**
+* Get the path to the configuration file.
+*/
+var getConfigFile = function() {
     try {
-        which.sync('vim');
+        return fs.readFileSync('./config', 'utf8');
     } catch (e) {
-        throw 'Your $EDITOR is not set and you don\'t have vim installed.\n' +
-            'Please either set your env $EDITOR or install vim.';
+        if (e.code !== 'ENOENT') {
+            throw e;
+        }
+        require('./First.js')
+            .run(start);
     }
+    return null;
 }
 
-// Check for the existence of the config file.
-try {
-    var config = JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'utf8'));
-} catch (error) {
-    if (error.code !== 'ENOENT') {
-        throw error;
-    }
-    throw 'You must create the config file \'config.json\'.\nSee example.config.json';
-}
+checkSudo();
+checkEditor();
 
-require('./PluginLoader.js').run(config);
+var config = getConfigFile();
+
+if (config !== null) {
+    start(config);
+}
