f98058cf6bd04939715dcdfe5644bd3c1bbaae58
PNikray <poujanik@gmail.com>
2015-04-23 14:04:05 +0200

    First.js is now fully tested

--- test/FirstTest.js
+++ test/FirstTest.js
@@ -1,8 +1,54 @@
-var assert = require('assert');
-describe('First', function(){
-    describe('#run', function(){
-        it('todo', function(){
-            assert(false);
+var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+var config = require('config');
+var proxyquire = require('proxyquire').noCallThru();
+var promptStub = {
+    multi: function() {}
+};
+var keytarStub = {
+    addPassword: function() {}
+};
+var fsStub = {
+    mkdirSync: function() {},
+    writeFileSync: function() {}
+};
+
+var First = proxyquire('../src/First.js', {
+    'cli-prompt': promptStub,
+    keytar: keytarStub,
+    fs: fsStub
+});
+
+describe('First', function() {
+    // Disable console.log
+    var log = console.log;
+    console.log = function() {};
+    after(function() {
+        console.log = log;
+    });
+
+    describe('#run', function() {
+        it('Should call keytar and fs correctly', function(done) {
+            promptStub.multi = function(questions, cb) {
+                questions.should.have.lengthOf(3);
+                cb({
+                    username: 'test.me',
+                    password: 'secret',
+                    host: 'example.com'
+                });
+            };
+            fsStub.mkdirSync = function(dir){
+                dir.should.be.equal(home + '/' + config.location);
+            };
+            fsStub.writeFileSync = function(path, file) {
+                path.should.be.equal(home + '/' + config.location + config.config);
+                file.should.not.have.property('password');
+            };
+            keytarStub.addPassword = function(profile, username, password){
+                profile.should.be.equal('jira-terminal');
+                password.should.be.equal('secret');
+                username.should.be.equal('test.me');
+            };
+            First.run(done);
         });
     });
 });
